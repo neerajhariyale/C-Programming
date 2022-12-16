a9338eb727eb81a26c54b9604e5451d327f21393
neerajhariyale <115395529+neerajhariyale@users.noreply.github.com>
2022-12-16 10:04:09 +0530

    Starpattern

---
+++ starpattern.c
@@ -0,0 +1,17 @@
+//star pattern
+
+#include<stdio.h>
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=3;i++)
+    {
+        for(j=1;j<=4;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
