d365d780f7d61ae1f2a62014a3cd0e4e11f880f4
neerajhariyale <115395529+neerajhariyale@users.noreply.github.com>
2022-12-28 09:39:24 +0530

    stairs like alphabet print

---
+++ nestedfor1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int i , j;
+
+    for (i = 65 ; i<=68 ; i++)
+    {
+        for(j = 65 ;j<=i ; j++)
+        {
+            printf("%c ",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
